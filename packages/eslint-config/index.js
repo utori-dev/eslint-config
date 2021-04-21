b206b8a14e01cf76df5ee0b22a3480c72f799c55
Michelle Miller <chellimiller@users.noreply.github.com>
2021-04-21 14:07:35 -0700

    #1 Initialize rule files

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,2 +1,13 @@
 module.exports = {
+  extends: [
+    './rules/best-practices',
+    './rules/errors',
+    './rules/es6',
+    './rules/imports',
+    './rules/node',
+    './rules/strict',
+    './rules/style',
+    './rules/variables',
+  ].map(require.resolve),
+  rules: {},
 };
